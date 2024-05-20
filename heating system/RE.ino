b4029172f5181a13cbb4c6a112470c0b32203c87
Changmin LEE <ckdals3084@naver.com>
2024-05-20 20:45:19 +0900

    code 오류 수정 완료

--- heating system/RE.ino
+++ heating system/RE.ino
@@ -1,24 +1,29 @@
-void heating_system()
+//코드 상 아무 문제 없음. 회로 연결이 문제.
+
+
+#define OC2A 0x08
+#define Relay_controll (1<<PB0) //릴레이 제어하는 핀은 8번핀.
+uint16_t temperature=22;
+
+
+void setup()
 {
-    #define OC0A 0b01000000 //6번핀 사용  5,6번핀은 쿨러 회전을 위한 핀.
-    #define OC0B 0b00100000 //5번핀 사용
-    #define Relay_controll (1<<PB0) //릴레이 제어하는 핀은 8번핀.
-    uint16_t temperature=17.0;
-    DDRB|=Relay_controll; //릴레이 제어하는 핀-> 세라믹 히터 on//off
-    DDRD|=(OC0A)|(OC0B); // 쿨러 회전을 위한 핀 설정. PWM을 사용해야 하기 때문에 꼭 해당 핀 사용
+    DDRB|=Relay_controll|OC2A; //릴레이 제어하는 핀-> 세라믹 히터 on//off
     //타이머 카운터 작동 모드 Fast PWM으로 설정.
-    TCCR0A|=(1<<WGM01)|(1<<WGM00);
-    TCCR0B|=(0<<WGM02);
+    TCCR2A|=(1<<WGM21)|(1<<WGM20);
     //타이머 카운터가 동작하기 위해 사용되는 클럭 선택
-    TCCR0B|=(1<<CS02)|(1<<CS01)|(1<<CS00);
-
+    TCCR2B|=(1<<CS22)|(1<<CS21)|(1<<CS20);
     //OCOA의 출력모드 설정
-    TCCR0A|=(1<<COM0A1)|(0<<COM0A0);
+    TCCR2A|=(1<<COM2A1);
     //OC0B의 출력 모드 설정
-    TCCR0A|=(1<<COM0B1)|(0<<COM0B0);
+    TCCR2A|=(1<<COM2B1);
+    //인터럽트 활성화
+    TIMSK2|=(1<<OCIE2A)|(1<<OCIE2B);
+}
 
 
-
+void loop()
+{
     if (temperature<=18.0)
     {
         //릴레이 on->세라믹 히터 on
@@ -27,8 +32,8 @@ void heating_system()
         SREG&=~1<<SREG_I; //SREG레지스터의 I비트를 clear.->인터럽트 사용 x
 
         //듀티 사이클 설정을 위한 OCR0A,OCR0B값 설정. 0이면 duty cycle 0. 255이면 duty cycle 100.
-        OCR0A=0;
-        OCR0B=0;
+        OCR2A=0;
+        
     }
     else if (temperature>18.0 && temperature<23.0)
     {
@@ -37,28 +42,18 @@ void heating_system()
         SREG&=~(1<<SREG_I); //SREG레지스터의 I비트를 clear.->인터럽트 사용 x
 
         //듀티 사이클 설정을 위한 OCR0A,OCR0B값 설정. 0이면 duty cycle 0. 255이면 duty cycle 100.
-        OCR0A=0;
-        OCR0B=0;
+        OCR2A=10;
+        
     }
 
     else
     {
-        PORTB&=~Relay_controll;
+        PORTB&=~Relay_controll; //세라믹 히터 꺼줌
         
         SREG|=1<<SREG_I; //SREG레지스터의 I비트를 set.->인터럽트 사용 o
 
         //듀티 사이클 설정을 위한 OCR0A,OCR0B값 설정. 0이면 duty cycle 0. 255이면 duty cycle 100.
-        OCR0A=128; 
-        OCR0B=0;
-
+        OCR2A=255;
+       
     }
 }
-
-ISR(TIMER0_COMPA_vect) 
-{
-    //인터럽트가 발생했을 때 어떤 상황을 발생시킬 지 정의 후 작성 예정.
-    //부저 이용해 팬 켜질 때 소리 한 번 나게 할 수 있다.
-}  
-
-
-   
