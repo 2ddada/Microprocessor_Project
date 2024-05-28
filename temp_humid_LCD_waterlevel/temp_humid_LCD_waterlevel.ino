e96b91be62225d6c12b1a6c182740c0732b4a21f
2ddada <149673030+2ddada@users.noreply.github.com>
2024-05-28 16:25:11 +0900

    temp, humid, lcd, waterpump combine fix

--- temp_humid_LCD_waterlevel/temp_humid_LCD_waterlevel.ino
+++ temp_humid_LCD_waterlevel/temp_humid_LCD_waterlevel.ino
@@ -5,6 +5,10 @@
 // DHT-22 센서는 digital pin
 
 
+//waterlevel 센서
+// 일단 하나 새거 주문하긴 했는데.
+// 기본적으로, num은 인터럽트가 생기는 주기(1초)마다 1씩 커지는 것으로 잡아 두었음. 
+
 
 //---------------------------header files---------------------
 
@@ -22,6 +26,17 @@
 #define DHTPIN 2        // SDA 핀의 설정
 
 
+#define Waterpump_Output_PIN_1 3 // 포트b의 3번째 핀
+#define Waterpump_Output_PIN_2 6 // 포트d의 6번째 핀
+#define Temperature_Input_PIN 0 // 온도 읽어올 핀
+#define Waterlevel_Input_PIN 2 // 수위 읽어올 핀
+
+#define NUM_LEDS 3
+#define LED_Output_PIN_1 0
+#define LED_Output_PIN_2 1
+#define LED_Output_PIN_3 2
+
+
 
 //-------------------variables------------------------------------------
 
@@ -32,6 +47,28 @@ int8_t temperature, humidity;
 bool water_refill;
 
 
+volatile int num = 0;
+
+
+//----------------------------------ISR-------------------------------------------------
+
+ISR(TIMER1_COMPA_vect) {
+  num++;
+  Serial.print("num check : ");
+  Serial.println(num);
+  // 5초동안 펌프를 켜기. 작동 시간 바꾸고 싶으면 아래 숫자만 바꿔주면 끝.
+  if (num > 10 && num <= 15) {
+    PORTB &= ~(1 << Waterpump_Output_PIN_1);
+    if (num == 15) {
+      num = 0;
+    }
+  } else {
+    PORTB |= (1 << Waterpump_Output_PIN_1);
+  }
+  Serial.print("waterpump check : ");
+  Serial.println(PORTB & (1 << Waterpump_Output_PIN_1));
+
+}
 //------------------------------setup functions----------------------------------------------------
 
 //Serial communication setup
@@ -86,6 +123,29 @@ void init_ADC(){
 }
 
 
+void pump_setup() {
+  DDRB |= (1 << Waterpump_Output_PIN_1);
+  DDRD |= (1 << Waterpump_Output_PIN_2);
+  PORTB &= ~(1 << Waterpump_Output_PIN_1); // 처음에는 꺼줌
+  PORTD &= ~(1 << Waterpump_Output_PIN_2);
+
+  cli();
+
+  // 타이머 1 설정 - CTC 모드, 프리스케일러 1024
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10); // CTC 모드, 프리스케일러 1024
+  OCR1A = 15624; // 1초마다 인터럽트 발생
+  TIMSK1 |= (1 << OCIE1A); // 타이머 1 비교 매치 인터럽트 활성화
+
+  sei();
+}
+
+void init_LED() {
+  DDRB |= (1 << LED_Output_PIN_1) | (1 << LED_Output_PIN_2) | (1 << LED_Output_PIN_3);
+}
+
+
 //-----------------------action------------------------------------------------
 
 void display_action(int8_t temperature, int8_t humidity, bool water_refill)
@@ -119,21 +179,36 @@ void display_action(int8_t temperature, int8_t humidity, bool water_refill)
 }
 
 
+int read_ADC(uint8_t pin) {
+  // ADC 채널 설정
+  ADMUX = (ADMUX & 0xF0) | (pin & 0x0F);
+  // ADC 변환 시작
+  ADCSRA |= (1 << ADSC);
+  // 변환 완료 대기
+  while (ADCSRA & (1 << ADSC));
+  // 결과 반환
+  return ADC;
+}
+
+
 // 온도체크
 // 일단은 interrupt 안하고 간단히 busywait으로 구현
 // busywait함수를 loop함수에 직접적으로 넣어야하나 아니면 개별 함수마다 넣어야하나?
 // 나중에 interrupt 구현하면 상관없을거같긴 한데 일단은 개별 함수마다 구현해야겠다
 int8_t read_temperature(){
-  // 뭐 지금 당장은 ADC0으로 들어오게 한다 해야겠다
-  // !!!!!!!!!!!!!!!!!!통합할때 수정 필요!!!!!!!!!!!!!!!!!!!
-  ADMUX |= (0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (0 << MUX0);
+  // // 뭐 지금 당장은 ADC0으로 들어오게 한다 해야겠다
+  // // !!!!!!!!!!!!!!!!!!통합할때 수정 필요!!!!!!!!!!!!!!!!!!!
+  // ADMUX |= (0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (0 << MUX0);
 
-  ADCSRA |= (1 << ADSC); // ADC 변환 시작
+  // ADCSRA |= (1 << ADSC); // ADC 변환 시작
 
-  // CPU가 ADSC 비트 1로 바꿔줄때까지 busywait 
-  while (ADCSRA & (1 << ADSC));
-  // 8비트 레지스터 두개 합쳐서 16비트의 값으로 바꾸고 temp_temperature 변수에 저장 (지금은 전압값, 0~1023사이의 값)
-  uint16_t temp_temperature = (uint16_t)ADCL | ((uint16_t)(ADCH) << 8);
+  // // CPU가 ADSC 비트 1로 바꿔줄때까지 busywait 
+  // while (ADCSRA & (1 << ADSC));
+  // // 8비트 레지스터 두개 합쳐서 16비트의 값으로 바꾸고 temp_temperature 변수에 저장 (지금은 전압값, 0~1023사이의 값)
+  // uint16_t temp_temperature = (uint16_t)ADCL | ((uint16_t)(ADCH) << 8);
+
+  uint16_t temp_temperature = read_ADC(Temperature_Input_PIN);
+
 
   // 이다음에 실제 온도값으로 변경
   // 계산과정에서 정수말고 실수로 될 수 도 있으니 float하고
@@ -147,6 +222,49 @@ int8_t read_humidity(){
   return (int8_t) dht.readHumidity();
 }
 
+int8_t read_temperature_digital(){
+  return (int8_t) dht.readTemperature();
+}
+
+
+void waterlevel_check() {
+
+  // ADCSRA |= (1 << ADSC);
+  // while (ADCSRA & (1 << ADSC));
+  // uint16_t value = ADC;
+
+  uint16_t value = read_ADC(Waterlevel_Input_PIN);
+
+  if (value < 400) {
+    PORTB |= (1 << LED_Output_PIN_1) | (1 << LED_Output_PIN_2) | (1 << LED_Output_PIN_3);
+    delay(500);
+    PORTB &= ~((1 << LED_Output_PIN_1) | (1 << LED_Output_PIN_2) | (1 << LED_Output_PIN_3));
+    delay(500);
+
+    // else if 가 안돌아가는데 왜냐면 조금이라도 닿기만 하면 바로 1023이 나와서 그럼.....하 ...
+    // 그럼 1023일때만 세개 다 켜지고 애매한 값일때는 두개만 켜지도록 해보자
+  } else if (value == 1023) {
+    PORTB |= (1<< LED_Output_PIN_1) | (1 << LED_Output_PIN_2) | (1 << LED_Output_PIN_3);
+  } else {
+    PORTB &= ~(1 << LED_Output_PIN_3);  // 세번째 led는 끄고 2개만 키도록
+    PORTB |= (1 << LED_Output_PIN_1) | (1 << LED_Output_PIN_2);
+  }
+
+  Serial.print("waterlevel sensor value : ");
+  Serial.println(value);
+  
+}
+
+void execute_waterlevel() {
+  static unsigned long lastMillis = 0;
+  if (millis() - lastMillis >= 1000) {
+    lastMillis = millis();
+    waterlevel_check();
+  }
+
+  //Serial.print("num  : ");
+  //Serial.println(num);
+}
 
 
 void serial_print(int8_t temperature, int8_t humidity){
@@ -172,20 +290,25 @@ void setup() {
   init_ADC();
   init_insideLED();
   dht.begin();
+  pump_setup();
+  init_LED();
 }
 
 int testcount=0;
 void loop() {
   // put your main code here, to run repeatedly:
+
+
   temperature = (int8_t) read_temperature();
   humidity = (int8_t) read_humidity();
-  if (temperature > 25) PORTB |= led_insdie_pin;
-  else PORTB &= ~led_insdie_pin;  
+  display_action(temperature, humidity, water_refill);
+  serial_print(temperature, humidity);  
+
+  execute_waterlevel();
+
 
   if(testcount++%2==0) water_refill=true;
   else water_refill=false;
-  display_action(temperature, humidity, water_refill);
-  serial_print(temperature, humidity);
 
 
   delay(1000); // 2초마다 업데이트
