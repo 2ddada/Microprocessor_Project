d8b6d25c517b5ac76df60c14144a24151095a3a4
Changmin LEE <ckdals3084@naver.com>
2024-06-05 15:06:02 +0900

    add bluetooth code

--- heating system/bluetooth mode.c
+++ heating system/bluetooth mode.c
@@ -1,14 +1,23 @@
+#include <SoftwareSerial.h>
+
+// SoftwareSerial 객체를 생성합니다.
+// RX_PIN과 TX_PIN을 실제 사용하고 있는 핀 번호로 바꿔야 합니다.
+SoftwareSerial btSerial(10, 11); // RX, TX
 
 //user_mode=0 -> 자동화모드 user_mode=1-> 사용자모드
 int user_mode=0; //처음에는 자동화모드로 설정
 char c='a'; //처음에는 자동화 모드 설정
+
+void setup(){
+
+}
 
 void loop() {
 
     if (btSerial.available()){  //블루투스로 문자 받아옴
         c=btSerial.read();
 
-    if (user_mode==0&c!='a')  //자동화모드에서 다른 문자가 선택되면 무시하기 위한 코드. 받아온 문자를 다시 a로 바꿔줌.
+    if (user_mode==0 & c!='a')  //자동화모드에서 다른 문자가 선택되면 무시하기 위한 코드. 받아온 문자를 다시 a로 바꿔줌.
         c='a';
 
     if (c=='a')  //자동화모드가 선택된다면 원래 루프 함수 돌림
@@ -21,7 +30,7 @@ void loop() {
     else //자동화모드 버튼 말고 다른 것이 눌렸을 때
     {   
         if (c=='b') //사용자모드 버튼이 눌렸다면 user_mode가 1이 되게 하여, 사용자모드 활성화
-            user_mode+=1;
+            user_mode=1;
         if (user_mode==1) //사용자모드로 선택되어 있다면, 환기 워터펌프 히팅 쿨링 관련 버튼 눌렀을 때 활성화.
         {
             switch (c)
